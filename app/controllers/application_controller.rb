f4921d8e1bbd10650170bdac1239ce1e9a258e68
Sam McAlilly <smcalilly@gmail.com>
2019-11-18 12:29:39 -0600

    fixed the http logger bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
-  require 'http_logger'
   before_action :authenticate_user!
+  before_action :enable_http_logger
+
+  private
+  def enable_http_logger
+    if Rails.env.development?
+      require 'http_logger'
+    end
+  end
 end
