ed2254b720656feb27fd4e09096098aa6dffa662
Sam McAlilly <smcalilly@gmail.com>
2019-11-19 15:14:15 -0600

    bug fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
     @current_user = AuthorizeApiRequest.call(request.headers).result
     puts @current_user
     render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+  end
 
   def enable_http_logger
     if Rails.env.development?
