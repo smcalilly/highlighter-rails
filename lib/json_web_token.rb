2ab14227616425a0ce53ec013af013bcf5777f4b
Sam McAlilly <smcalilly@gmail.com>
2019-11-19 22:30:42 -0600

    fixed production env credential issue

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,15 +1,18 @@
 class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
+      puts 'encode'
+      puts payload
       payload[:exp] = exp.to_i
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      JWT.encode(payload, Rails.application.credentials.jwt_secret_key)
     end
 
     def decode(token)
-      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      body = JWT.decode(token, Rails.application.credentials.jwt_secret_key)[0]
       HashWithIndifferentAccess.new body
-   rescue
-     nil
+    rescue JWT::ExpiredSignature, JWT::DecodeError
+      false
     end
   end
 end
+
