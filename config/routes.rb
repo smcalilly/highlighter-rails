bcdb0d33a5c3bb66ad78a895e016c4cca34bcbd0
Sam McAlilly <sammcalilly@gmail.com>
2020-01-26 16:53:27 -0600

    redirect logged in user to the main page if they visit the root path. seems counter intutitve to have a piece of the routing in the controller like this, but it's my solution for now, as i figure out a better way to do it with the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  
-  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
+  # setup for the json api
+  devise_for :users, :controllers => { sessions: 'sessions', registrations: 'registrations' }
   post 'authenticate', to: 'authentication#authenticate'
 
+  # landing page for marketing
   root to: 'marketing#home'
-  get '/user' => "highlights#index", :as => :user_root
+
+  
+  #get '/user' => "highlights#index", :as => :user_root
+
+  # authenticated :user do
+  #   root :to => "highlights#index"
+  # end
   
   resources :highlights
   resources :sources, only: [:index, :show, :new, :create, :destroy]
