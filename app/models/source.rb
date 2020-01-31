1115edc34d3b7b0e6cf6d487708bb01dcf64b331
Sam McAlilly <sammcalilly@gmail.com>
2020-01-30 22:55:32 -0600

    fixed link overflow issue and added destroy dependency for associated children in the source model

--- app/models/source.rb
+++ app/models/source.rb
@@ -2,6 +2,6 @@ class Source < ApplicationRecord
     validates :location, presence: true
     validates_uniqueness_of :location, scope: :user_id
     belongs_to :user
-    has_many :highlights
+    has_many :highlights, dependent: :destroy
     accepts_nested_attributes_for :highlights
 end
