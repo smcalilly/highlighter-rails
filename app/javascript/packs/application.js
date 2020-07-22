564e08bc85e3a5d5b09b558fcdb71026bf574a67
Sam McAlilly <sammcalilly@gmail.com>
2020-07-22 15:50:45 -0500

    styling the text editor. started to add stimulus but getting ahead of myself

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,3 +20,10 @@ import "../../assets/stylesheets/application"
 
 require("trix")
 require("@rails/actiontext")
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("../controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
