d298601fb272b49d880d2319ad81d969fee0e272
Sam McAlilly <sammcalilly@gmail.com>
2020-07-21 22:35:12 -0500

    got tailwind up

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 // postcss.config.js
 let environment = {
   plugins: [
-    require('tailwindcss')('./app/stylesheets/tailwind.config.js'),
+    require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
